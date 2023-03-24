f313c203ea682c868495328aa240f6a2bb4dfabc
Baizey <lassemoerk.mrk@gmail.com>
2023-03-24 23:54:32 +0100

    #minor add support for global styling

--- src/core/elements.tsx
+++ src/core/elements.tsx
@@ -1,31 +1,43 @@
 import {ElementProps} from "./props";
-import {classname, wrapCssInClass} from "./utils";
+import {classname, mergeStyling, wrapCssInClass} from "./utils";
 import * as React from "preact/compat";
+import {StyledTheme} from "./theme";
 
 export const Div = ({children, className, styling, ...props}: ElementProps & React.HTMLAttributes<HTMLDivElement>) => {
     const name = classname();
     return <div {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </div>;
 }
 
-export const Span = ({children, className, styling, ...props}: ElementProps & React.HTMLAttributes<HTMLSpanElement>) => {
+export const Span = ({
+                         children,
+                         className,
+                         styling,
+                         ...props
+                     }: ElementProps & React.HTMLAttributes<HTMLSpanElement>) => {
     const name = classname();
     return <span {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </span>;
 }
 
-export const Label = ({children, className, styling, ...props}: ElementProps & React.HTMLAttributes<HTMLLabelElement>) => {
+export const Label = ({
+                          children,
+                          className,
+                          styling,
+                          ...props
+                      }: ElementProps & React.HTMLAttributes<HTMLLabelElement>) => {
     const name = classname();
     return <label {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </label>;
 }
 
+mergeStyling(StyledTheme.getGlobalStyling())
 export const Header1 = ({
                             children,
                             className,
@@ -35,7 +47,7 @@ export const Header1 = ({
     const name = classname();
     return <h1 {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </h1>;
 }
 
@@ -48,7 +60,7 @@ export const Header2 = ({
     const name = classname();
     return <h2 {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </h2>;
 }
 
@@ -61,7 +73,7 @@ export const Header3 = ({
     const name = classname();
     return <h3 {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </h3>;
 }
 
@@ -74,23 +86,33 @@ export const UnorderedList = ({
     const name = classname();
     return <ul {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </ul>;
 }
 
-export const ListItem = ({children, className, styling, ...props}: ElementProps & React.HTMLAttributes<HTMLLIElement>) => {
+export const ListItem = ({
+                             children,
+                             className,
+                             styling,
+                             ...props
+                         }: ElementProps & React.HTMLAttributes<HTMLLIElement>) => {
     const name = classname()
     return <li {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </li>;
 };
 
-export const Link = ({children, className, styling, ...props}: ElementProps & React.HTMLAttributes<HTMLAnchorElement>) => {
+export const Link = ({
+                         children,
+                         className,
+                         styling,
+                         ...props
+                     }: ElementProps & React.HTMLAttributes<HTMLAnchorElement>) => {
     const name = classname();
     return <a {...props} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(name, styling)}
+        {styling && wrapCssInClass(name, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </a>;
 }
 
@@ -104,7 +126,7 @@ export const Button = ({
     const name = classname();
     return <button {...props} type={type} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(`${name}[type="${type}"]`, styling)}
+        {styling && wrapCssInClass(`${name}[type="${type}"]`, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </button>;
 }
 
@@ -118,6 +140,6 @@ export function Input({
     const name = classname()
     return <input{...props} type={type} className={`${className},${name}`}>
         {children}
-        {styling && wrapCssInClass(`${name}[type="${type}"]`, styling)}
+        {styling && wrapCssInClass(`${name}[type="${type}"]`, mergeStyling(styling, StyledTheme.getGlobalStyling()))}
     </input>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "preact/compat";
-import {Div} from "./core/elements";
-import {WithStyle} from "./core/props";
-import {css, mergeStyling} from "./core/utils";
 
 export * from './core/elements'
 export * from './core/props'
 export * from './core/utils'
+export * from './core/theme'
