29b89dabdb1f66ad5b000e70d05061849c626789
Baizey <lassemoerk.mrk@gmail.com>
2023-04-23 04:07:26 +0200

    #patch include HTMLAttributes<EventTarget> in ElementProps

--- src/core/props.ts
+++ src/core/props.ts
@@ -1,8 +1,8 @@
 import {PreactDOMAttributes} from "preact";
-import {PropsWithChildren} from "preact/compat";
+import {HTMLAttributes, PropsWithChildren} from "preact/compat";
 
 export type ClassName = `styled-${string}-${number}` | `styled-${string}-${number}[type="${string}"]`
 
 export type WithStyle = { styling?: string } & PropsWithChildren
 
-export type ElementProps = WithStyle & PreactDOMAttributes
+export type ElementProps = WithStyle & PreactDOMAttributes & HTMLAttributes<EventTarget>
