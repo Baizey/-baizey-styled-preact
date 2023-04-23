ca9ccab0733dd0ebe46d2a3b47d82e616b4cb9a5
Baizey <lassemoerk.mrk@gmail.com>
2023-04-23 04:17:37 +0200

    #patch update to individual prop types

--- src/core/props.ts
+++ src/core/props.ts
@@ -5,4 +5,15 @@ export type ClassName = `styled-${string}-${number}` | `styled-${string}-${numbe
 
 export type WithStyle = { styling?: string } & PropsWithChildren
 
-export type ElementProps = WithStyle & PreactDOMAttributes & HTMLAttributes<EventTarget>
+export type ElementProps = WithStyle & PreactDOMAttributes
+export type DivProps = ElementProps & HTMLAttributes<HTMLDivElement>
+export type SpanProps = ElementProps & HTMLAttributes<HTMLSpanElement>
+export type LinkProps = ElementProps & HTMLAttributes<HTMLAnchorElement>
+export type ButtonProps = ElementProps & HTMLAttributes<HTMLButtonElement>
+export type InputProps = ElementProps & HTMLAttributes<HTMLInputElement>
+export type LabelProps = ElementProps & HTMLAttributes<HTMLLabelElement>
+export type SelectProps = ElementProps & HTMLAttributes<HTMLSelectElement>
+export type OptionProps = ElementProps & HTMLAttributes<HTMLOptionElement>
+export type HeaderProps = ElementProps & HTMLAttributes<HTMLHeadingElement>
+export type ListProps = ElementProps & HTMLAttributes<HTMLUListElement>
+export type ListItemProps = ElementProps & HTMLAttributes<HTMLLIElement>
